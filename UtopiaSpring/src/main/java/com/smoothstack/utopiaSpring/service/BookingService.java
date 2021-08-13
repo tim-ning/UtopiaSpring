1bd7988b84e7f4db8edc03d8ac5d5c131c0c656a
markmocek <markmocek@yahoo.com>
2021-08-13 09:54:56 -0700

    bookingdao and service

---
+++ UtopiaSpring/src/main/java/com/smoothstack/utopiaSpring/service/BookingService.java
@@ -0,0 +1,34 @@
+package com.smoothstack.utopiaSpring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.smoothstack.utopiaSpring.dao.BookingDao;
+import com.smoothstack.utopiaSpring.model.Booking;
+
+@Service
+public class BookingService {
+
+	@Autowired
+	private BookingDao bookingDao;
+
+	public List<Booking> readAllBookings() {
+		return bookingDao.findAll();
+	}
+
+	public Optional<Booking> readBookingById(Long id) {
+		return bookingDao.findById(id);
+	}
+
+	public Booking addBooking(Booking newBooking) {
+		return bookingDao.save(newBooking);
+	}
+
+	public void deleteFlight(Long id) {
+		bookingDao.deleteById(id);
+	}
+	
+}
